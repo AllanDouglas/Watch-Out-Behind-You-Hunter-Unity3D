dac7f8cd44413053756f60bf7e6399aaaf18948c
Allan Douglas <allandouglas@live.com>
2021-04-17 15:29:52 -0300

    new main menu start button

---
+++ Assets/_Game/Scripts/Implementations/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace WOBH
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] GameObject controllerTips;
+
+        private void Awake()
+        {
+#if UNITY_ANDROID || UNITY_IOS
+            controllerTips.SetActive(false);
+#endif
+        }
+
+    }
+}
